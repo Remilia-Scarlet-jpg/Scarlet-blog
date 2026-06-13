7caea245fa350b2c236a855bcb1d28c79cb1359b
developer03 <developer03@qq.com>
2026-06-14 01:01:26 +0800

    debug: test without SSL to narrow down TLS issue

--- src/com/scarletblog/util/DBUtil.java
+++ src/com/scarletblog/util/DBUtil.java
@@ -19,8 +19,7 @@ public class DBUtil {
         "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME
         + "?useUnicode=true&characterEncoding=UTF-8"
         + "&serverTimezone=Asia/Shanghai"
-        + "&sslMode=REQUIRED"
-        + "&tlsVersions=TLSv1.2,TLSv1.3"
+        + "&sslMode=DISABLED"
         + "&connectTimeout=20000"
         + "&socketTimeout=20000";
     private static final String USER = DB_USER;
